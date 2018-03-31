48874934cea5349b26db0dd91e9f160b30db6a50
Claud <wangzhaoyunx@163.com>
2018-03-31 08:56:18 +0800

    2

--- Component_A/LittleComponent_A/Targets/Target_ComponentA.h
+++ Component_A/LittleComponent_A/Targets/Target_ComponentA.h
@@ -2,7 +2,7 @@
 //  Target_ComponentA.h
 //  Component_A
 //
-//  Created by Zhaoyun Wang on 2018/3/30.
+//  Created by Zhaoyun Wang on 2018/3/31.
 //  Copyright © 2018年 Zhaoyun Wang. All rights reserved.
 //
 
